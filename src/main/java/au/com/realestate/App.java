a5a1484df83e7788c68fb4b732991b81f66aa6e5
Ryan Brown <piston47@gmail.com>
2014-11-17 21:16:55 +1100

    Initital Commit - Added maven layout and logging

---
+++ src/main/java/au/com/realestate/App.java
@@ -0,0 +1,17 @@
+package au.com.realestate;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOG = Logger.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
